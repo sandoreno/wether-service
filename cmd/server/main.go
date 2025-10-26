0f5115a253134020bb1036c02ee7488da2b3a113
sandoreno <ayazkhas@mail.ru>
2025-10-26 23:22:59 +0300

    add http server

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		panic(err)
+	}
+}
